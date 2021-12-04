0894b2d121f8cfb8e8b099ca186cc0f31642f13b
João Leonardo Morganti <joaosilva.e-deploy@natura.net>
2021-12-04 11:54:46 -0300

    Create html with bootstrap and jQuery frontend

---
+++ frontend/js/scripts.js
@@ -0,0 +1,79 @@
+$(document).ready(function () {
+
+    $("#btn_inserir").click(function () {
+        valorOrigem= $("#input_inserir_origem").val();
+        valorDestino = $("#input_inserir_destino").val();
+        valorPeso = $("#input_inserir_peso").val();
+        valorData = $("#input_inserir_data").val();
+        $.ajax({
+            url: 'http://localhost:5001/encomenda/insert',
+            type: 'POST',
+            crossDomain: true,
+            dataType: 'json',
+            data: {
+                origem: valorOrigem,
+                destino: valorDestino,
+                peso: valorPeso,
+                data: valorData,
+            },
+            success: function (result, status, xhr) { alert(status); },
+            error: function () { alert("error"); },
+        });
+    });
+
+    $("#btn_listar").click(function () {
+        $.ajax({
+            url: 'http://localhost:5000/aluno/listar',
+            type: 'GET',
+            crossDomain: true,
+            dataType: 'json',
+            success: function (result, status, xhr) {
+                table = '<table class="table" border="1">';
+                table += '<tr><th>id</th><th>nome</th><th>curso</th><th>nascimento</th></tr>'
+                $.each(result, function (indice, obj) {
+					table += `<tr><td>${obj.id}</td><td>${obj.nome}</td><td>${obj.curso}</td><td>${obj.nascimento}</td></tr>`;
+                });
+                table += '</table>';
+                $("#div_listar").html(table);
+             },
+            error: function () { alert("error"); }
+        });
+    });
+
+    $("#btn_atualizar").click(function () {
+        valorId = $("#input_atualizar_id").val();
+        valorNome = $("#input_atualizar_nome").val();
+        valorCurso = $("#input_atualizar_curso").val();
+        valorNascimento = $("#input_atualizar_nascimento").val();
+        $.ajax({
+            url: 'http://localhost:5000/aluno/atualizar',
+            type: 'PUT',
+            crossDomain: true,
+            dataType: 'json',
+            data: {
+                id: valorId,
+                nome: valorNome,
+                curso: valorCurso,
+                nascimento: valorNascimento
+            },
+            success: function (result, status, xhr) { alert(status); },
+            error: function () { alert("error"); }
+        });
+    });
+
+    $("#btn_excluir").click(function () {
+        valorId = $("#input_excluir_id").val();
+        $.ajax({
+            url: 'http://localhost:5000/aluno/excluir',
+            type: 'DELETE',
+            crossDomain: true,
+            dataType: 'json',
+            data: {
+                id: valorId
+            },
+            success: function (result, status, xhr) { alert(status); },
+            error: function () { alert("error"); }
+        });
+    });
+
+});
